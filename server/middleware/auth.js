2cb6b5cd362854def82462698eb96619a8299bc5
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-16 01:30:35 +0530

    setup JWT middleware file

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    if (!token) return res.status(401).json({message: "Access denied. No token"});
+
+    try {
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(400).json({message: "Invalid token"});
+    };
+};
+
+const checlRole = (role) => (req, res, next) => {
+    if (req.user.role !== role)
+        return res.status(403).json({message: "Access denied. No role"});
+    next();
+}
+
+module.exports = { verifyToken, checkRole };
