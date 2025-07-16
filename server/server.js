cf965ae9ae6c60d0738f5a06989ebd48a7ea2b8f
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-16 06:07:19 +0530

    added admin and protected route for JWT testing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const { verifyToken, checkRole } = require("./middleware/auth");
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./routes/authRoutes"));
+
+app.get("/api/protected", verifyToken, (req, res) => {
+    res.json({ message: "Access granted", user: req.user });
+});
+  
+app.get("/api/admin-only", verifyToken, checkRole("admin"), (req, res) => {
+    res.json({ message: "Hello Admin!", user: req.user });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=> {console.log(`backend running @${PORT}`)});
